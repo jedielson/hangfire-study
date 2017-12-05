16e138b601681e028117e0579a307e7b30b025ce
Jedielson Nakonieczni <jedi_matind@hotmail.com>
2017-12-05 17:13:36 -0200

    Testando

---
+++ HangFire.Study.Web/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Hangfire;
+using Hangfire.Study.Jobs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HangFire.Study.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IEmailSender, EmailSender>();
+
+            services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("HangFireConnection")));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            app.UseMvc();
+
+            ExecuteJobs(app);
+        }
+
+        private void ExecuteJobs(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var mailSender = serviceScope.ServiceProvider.GetRequiredService<IEmailSender>();
+
+            }
+        }
+    }
+}
---
+++ src/Hangfire.Study.Jobs/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Hangfire.Common;
+using Microsoft.Extensions.Logging;
+
+namespace Hangfire.Study.Jobs
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IBackgroundJobClient _client;
+        private readonly IRecurringJobManager _jobManager;
+        private readonly ILogger<EmailSender> _logger;
+
+        public EmailSender(IBackgroundJobClient client, IRecurringJobManager jobManager, ILogger<EmailSender> _logger)
+        {
+            _client = client;
+            _jobManager = jobManager;
+            this._logger = _logger;
+
+            _jobManager.AddOrUpdate("Teste", Job.FromExpression(() => Lol()), Cron.Minutely());
+        }
+
+        public void Lol()
+        {
+            _logger.LogDebug("Executando o job aos {data}", DateTime.Now);
+        }
+    }
+
+    public interface IEmailSender
+    {
+    }
+}
